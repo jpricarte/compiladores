df619b9f7486e94bf944ffb7fcc0f496ee8afad7
Edu Fantini <edufantinicod@gmail.com>
2023-02-23 19:41:17 -0300

    Add type_infer()

--- e4/table.hh
+++ e4/table.hh
@@ -26,6 +26,21 @@ Type type_infer (Type symbol_1, Type symbol_2) {
 /*
  *  Dado dois simbolos, faz inferência e retorna o tipo.
  */
+
+    if (symbol_1 == symbol_2) {
+        if (symbol_1 == Type::INTEGER)
+            return Type::INTEGER;
+        if (symbol_1 == Type::FLOATING)
+            return Type::FLOATING;
+        if (symbol_1 == Type::BOOLEAN)
+            return Type::BOOLEAN;
+    }
+
+    if (symbol_1 == Type::FLOATING || symbol_2 == Type::FLOATING)
+        return Type::FLOATING;
+
+    if (symbol_1 == Type::INTEGER || symbol_2 == Type::INTEGER)
+        return Type::INTEGER;
 }
 
 struct SymbolTableStack {;
